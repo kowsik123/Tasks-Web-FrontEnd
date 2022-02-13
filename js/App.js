68a9cd001cdacb1fc4a4b0b359bf3b747ed27ab0
kowsik123 <77493681+kowsik123@users.noreply.github.com>
2022-02-13 08:38:01 +0530

    Update App.js

--- js/App.js
+++ js/App.js
@@ -6,13 +6,25 @@ $.ajaxSetup({
 
 let isGuest = false;
 let taskGroupManager = false;
-if( document.cookie.includes( "credentialId" ) ) {
-    const credentialId = document.cookie.substr( document.cookie.indexOf( "credentialId" )+13 , 5)
+const credentialId = getCookie( "credentialId" );
+if( credentialId ) {
     if ( credentialId==="guest" ) isGuest=true;
     taskGroupManager = new TaskGroupManager()
     ReactDOM.render(<App />, document.getElementById('root'))
 }
 else ReactDOM.render( <Login /> , document.getElementById('root') )
+
+function getCookie(cname) {
+  let name = cname + "="
+  let decodedCookie = decodeURIComponent(document.cookie)
+  let ca = decodedCookie.split(';')
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) == ' ') c = c.substring(1)
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length)
+  }
+  return null
+}
 
 function App() {
     console.log("app rendered");
